6aa653c7092cd47156fc2ba8b1bd2f34810a0b78
ayluc <lucas.branchu@live.fr>
2025-01-16 09:11:05 +0100

    Add files via upload

--- tp1progreseau/src/tp1progreseau/cliTexte1.java
+++ tp1progreseau/src/tp1progreseau/cliTexte1.java
@@ -12,19 +12,18 @@ public class cliTexte1 {
 		int p; // le port de connexion
 		String ch; // la chaîne envoyée
 		int l; // et sa longueur reçue
-		if (argu.length == 3) { // on récupère les paramètres
-			s=argu[0];
-			p=Integer.parseInt(argu[1]);
-			ch=argu[2];
-			try{// on connecte un socket
-				so = new Socket(s, p);
-				sortie = new PrintWriter(so.getOutputStream(), true);
-				entree = new BufferedReader(new InputStreamReader(so.getInputStream()));
-				sortie.println(ch); // on écrit la chaîne et le newline dans le canal de sortie
-				l = entree.read(); // on lit l’entier qui arrive
-				System.out.println("D’après le serveur la longueur de "+ch+" est "+l);
-				so.close(); // on ferme la connexion
-			} catch(UnknownHostException e) {System.out.println(e);}
-			catch (IOException e) {System.out.println("Aucun serveur n’est rattaché au port ");}
-		} else {System.out.println("syntaxe d’appel java cliTexte serveur port chaine_de_caractères\n");
-} } }
+		
+		s="127.0.0.1";
+		p=2500;
+		ch="test";
+		try{// on connecte un socket
+			so = new Socket(s, p);
+			sortie = new PrintWriter(so.getOutputStream(), true);
+			entree = new BufferedReader(new InputStreamReader(so.getInputStream()));
+			sortie.println(ch); // on écrit la chaîne et le newline dans le canal de sortie
+			l = entree.read(); // on lit l’entier qui arrive
+			System.out.println("D’après le serveur la longueur de "+ch+" est "+l);
+			so.close(); // on ferme la connexion
+		} catch(UnknownHostException e) {System.out.println(e);}
+		catch (IOException e) {System.out.println("Aucun serveur n’est rattaché au port ");}	
+} } 
