2d0823b5fb20312673840a5dc6a113a181373e24
Guyliane03 <103633824+Guyliane03@users.noreply.github.com>
2025-01-16 09:36:51 +0100

    Update Client.java

--- tp1progreseau/src/tp1progreseau/Client.java
+++ tp1progreseau/src/tp1progreseau/Client.java
@@ -4,33 +4,44 @@ import java.net.*;
 import java.io.*;
 
 public class Client {
-	private String s;
-	private int p;
-	private String ch;
-	private int l;
 	
-	public Client(String s, int p, String ch, int l) {
-		this.s = s;
-		this.p = p;
-		this.ch = ch;
-		this.l = l;
+	private String name;
+	private int port;
+	private String currentmessage;
+	private String postMessage;
+	private int lengthCurrentMessage;
+	private Socket socket;
+	
+	public Client(String name) {
+		this.name = name;
+	
 	}
 	
-	public void run() {
-		Socket so;
+	public void connect(int port) {
+		this.port = port;
 		BufferedReader entree;
 		PrintWriter sortie;
 		
-		try{// on connecte un socket
-			so = new Socket(s, p);
-			sortie = new PrintWriter(so.getOutputStream(), true);
-			entree = new BufferedReader(new InputStreamReader(so.getInputStream()));
-			sortie.println(ch); // on écrit la chaîne et le newline dans le canal de sortie
-			l = entree.read(); // on lit l’entier qui arrive
-			System.out.println("D’après le serveur la longueur de "+ch+" est "+l);
-			so.close(); // on ferme la connexion
-		} catch(UnknownHostException e) {System.out.println(e);}
+		try
+		{
+			this.socket = new Socket(name, this.port);
+//			sortie = new PrintWriter(so.getOutputStream(), true);
+//			entree = new BufferedReader(new InputStreamReader(so.getInputStream()));
+//			sortie.println(this.currentmessage); 
+//			this.lengthCurrentMessage = entree.read();
+//			System.out.println("D’après le serveur la longueur de "+this.currentmessage+" est "+this.lengthCurrentMessage);
+		} 
+		catch(UnknownHostException e) {System.out.println(e);}
 		catch (IOException e) {System.out.println("Aucun serveur n’est rattaché au port ");}
+	}
+	
+	public void disconnect() {
+		try
+		{
+			this.socket.close();
+		} 
+		catch(UnknownHostException e) {System.out.println(e);}
+		catch (IOException e) {System.out.println("Le socket n'est pas connecté");}
 	}
 	
 } 
