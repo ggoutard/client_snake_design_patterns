f0d00dcb6bfd27bf579be6912bce739575abe2e5
Guyliane03 <103633824+Guyliane03@users.noreply.github.com>
2025-01-16 09:19:55 +0100

    Add files via upload

---
+++ tp1progreseau/src/tp1progreseau/Serveur.java
@@ -0,0 +1,36 @@
+package tp1progreseau;
+
+import java.net.*;
+import java.io.*;
+
+public class Serveur {
+    private int port; 
+    private ServerSocket ecoute;
+
+    public Serveur(int port) {
+        this.port = port;
+    }
+
+    public void run() {
+        try {
+            ecoute = new ServerSocket(port);
+            System.out.println("Serveur mis en place sur le port " + port);
+
+            while (true) {
+                try (Socket so = ecoute.accept();
+                     BufferedReader entree = new BufferedReader(new InputStreamReader(so.getInputStream()));
+                     PrintWriter sortie = new PrintWriter(so.getOutputStream())) {
+
+                    String ch = entree.readLine();
+                    System.out.println("On a reçu : |" + ch + "|");
+                    sortie.write(ch.length());
+                    sortie.flush();
+                    System.out.println("On a envoyé : " + ch.length() + " et on a fermé la connexion");
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Problème\n" + e);
+        }
+    }
+
+}
