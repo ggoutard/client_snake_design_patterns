ad292f1ed7386d3a5c7b21e53871530c68e75727
ayluc <lucas.branchu@live.fr>
2025-01-16 09:28:50 +0100

    Update Serveur.java

--- tp1progreseau/src/tp1progreseau/Serveur.java
+++ tp1progreseau/src/tp1progreseau/Serveur.java
@@ -17,20 +17,24 @@ public class Serveur {
             System.out.println("Serveur mis en place sur le port " + port);
 
             while (true) {
-                try (Socket so = ecoute.accept();
-                     BufferedReader entree = new BufferedReader(new InputStreamReader(so.getInputStream()));
-                     PrintWriter sortie = new PrintWriter(so.getOutputStream())) {
+                try {
+                	Socket so = ecoute.accept();
+                
+                	BufferedReader entree = new BufferedReader(new InputStreamReader(so.getInputStream()));
+                    PrintWriter sortie = new PrintWriter(so.getOutputStream());
 
                     String ch = entree.readLine();
                     System.out.println("On a reçu : |" + ch + "|");
                     sortie.write(ch.length());
                     sortie.flush();
                     System.out.println("On a envoyé : " + ch.length() + " et on a fermé la connexion");
+                } catch (IOException e) {
+                	System.out.println("Problème\n" + e);
                 }
             }
-        } catch (IOException e) {
-            System.out.println("Problème\n" + e);
-        }
-    }
 
+       } catch (IOException e) {
+    	   System.out.println("Problème\n" + e);
+       }
+    }
 }
