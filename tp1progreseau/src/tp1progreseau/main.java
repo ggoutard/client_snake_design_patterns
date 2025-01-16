01c3556ac217904153285d9c079266ae1ac8a64e
Guyliane03 <103633824+Guyliane03@users.noreply.github.com>
2025-01-16 09:18:53 +0100

    Update main.java

--- tp1progreseau/src/tp1progreseau/main.java
+++ tp1progreseau/src/tp1progreseau/main.java
@@ -1,5 +1,13 @@
 package tp1progreseau;
 
+import tp1progreseau.Serveur;
+
 public class main {
-	
+	public static void main(String[] argu) {
+		
+		Serveur s = new Serveur(2500);
+		
+		s.run();
+		
+	}
 }
